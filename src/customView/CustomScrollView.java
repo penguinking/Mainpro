948add2130ba100bab69bc8f749f275862c4ead7
kingpenguin <1986_lp@163.com>
2013-10-26 10:10:52 +0800

    10-26修改，增加自定义ScrollView解决与viewpager冲突问题

---
+++ src/customView/CustomScrollView.java
@@ -0,0 +1,38 @@
+package customView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class CustomScrollView extends ScrollView{
+
+	private GestureDetector mGestureDetector;
+
+	public CustomScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mGestureDetector = new GestureDetector(context, (OnGestureListener) new YScrollDetector());
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		return super.onInterceptTouchEvent(ev)
+				&& mGestureDetector.onTouchEvent(ev);
+	}
+
+	class YScrollDetector extends SimpleOnGestureListener {
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2,
+				float distanceX, float distanceY) {
+			/**
+			 * 如果我们滚动更接近水平方向,返回false,让子视图来处理它
+			 */
+			return (Math.abs(distanceY) > Math.abs(distanceX));
+		}
+	}
+}
+
