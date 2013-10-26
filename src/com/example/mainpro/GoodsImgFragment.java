2be9617b805b93b42fbd4184d31f4e0961879dc7
kingpenguin <1986_lp@163.com>
2013-10-26 17:32:42 +0800

    修改界面布局

--- src/com/example/mainpro/GoodsImgFragment.java
+++ src/com/example/mainpro/GoodsImgFragment.java
@@ -1,39 +1,129 @@
 package com.example.mainpro;
 
-
-
 import com.loopj.android.image.SmartImageView;
 
+import controller.AnimationController;
+
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class GoodsImgFragment extends Fragment{
+public class GoodsImgFragment extends Fragment {
+
+	private SmartImageView img_hot_goods_right;
+	private SmartImageView img_hot_goods_center;
+	private SmartImageView img_hot_goods_left;
+	private String[] img_list;
+	private int i;
+	private String[] img_addr_list;
+	private int j = 0;
+	private View rootView;
+	private AnimationController animationController;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 
 	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 
-		View rootView = inflater.inflate(R.layout.fragment_goods_img, container,
+		rootView = inflater.inflate(R.layout.fragment_goods_img, container,
 				false);
-		int i = getArguments().getInt("no");// 获取ID值
-		String[] img_list = getArguments().getStringArray("info");
-		SmartImageView img_hot_goods_right = (SmartImageView)rootView.findViewById(R.id.img_hot_goods_right);
-		SmartImageView img_hot_goods_center = (SmartImageView)rootView.findViewById(R.id.img_hot_goods_center);
-		SmartImageView img_hot_goods_left = (SmartImageView)rootView.findViewById(R.id.img_hot_goods_left);
-		System.out.println(img_list[i]);
-		System.out.println(i);
-		img_hot_goods_right.setImageUrl(img_list[i]);
-		img_hot_goods_center.setImageUrl(img_list[i]);
-		img_hot_goods_left.setImageUrl(img_list[i+1]);
+		i = getArguments().getInt("no");// 获取ID值
+		img_list = getArguments().getStringArray("info");
+
+		img_hot_goods_right = (SmartImageView) rootView
+				.findViewById(R.id.img_hot_goods_right);
+		img_hot_goods_center = (SmartImageView) rootView
+				.findViewById(R.id.img_hot_goods_center);
+		img_hot_goods_left = (SmartImageView) rootView
+				.findViewById(R.id.img_hot_goods_left);
+		String img_addr = img_list[i];
+		img_addr_list = img_addr.split(";");
+		handler.postDelayed(runnable, 2000);
+
 		return rootView;
 	}
+
+	private Runnable runnable = new Runnable() {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			/**
+			 * AnimationSet animationSet1 = new AnimationSet(true); AnimationSet
+			 * animationSet2 = new AnimationSet(true); TranslateAnimation ta =
+			 * new TranslateAnimation( Animation.RELATIVE_TO_SELF, 0f,
+			 * Animation.RELATIVE_TO_SELF, -1f, Animation.RELATIVE_TO_SELF, 0f,
+			 * Animation.RELATIVE_TO_SELF, 0f); ta.setDuration(2000);
+			 * animationSet1.addAnimation(ta); animationSet1.setFillAfter(true);
+			 * ta = new TranslateAnimation(Animation.RELATIVE_TO_SELF, 1.0f,
+			 * Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0f,
+			 * Animation.RELATIVE_TO_SELF, 0f); ta.setDuration(2000);
+			 * animationSet2.addAnimation(ta); animationSet2.setFillAfter(true);
+			 * img_hot_goods_right.setAnimation();
+			 * //img_hot_goods_center.setAnimation(animationSet2);
+			 * //img_hot_goods_left.setAnimation(animationSet2);
+			 **/
+			final Animation animation = (Animation) AnimationUtils
+					.loadAnimation(rootView.getContext(),
+							android.R.anim.fade_in);
+			final Animation animation2 = (Animation) AnimationUtils
+					.loadAnimation(rootView.getContext(),
+							android.R.anim.slide_in_left);
+
+			System.out.println(img_list[i]);
+			switch (j) {
+			case (0):
+				img_hot_goods_right.setAnimation(animation);
+				img_hot_goods_center.setAnimation(animation);
+				img_hot_goods_left.setAnimation(animation);
+				img_hot_goods_right.setImageUrl(img_addr_list[0]);
+				img_hot_goods_center.setImageUrl(img_addr_list[1]);
+				img_hot_goods_left.setImageUrl(img_addr_list[2]);
+				// animationController.scaleIn(v, durationMillis, delayMillis);
+				j = j + 1;
+				break;
+			case (1):
+				img_hot_goods_right.setAnimation(animation);
+				img_hot_goods_center.setAnimation(animation);
+				img_hot_goods_left.setAnimation(animation);
+				img_hot_goods_right.setImageUrl(img_addr_list[1]);
+				img_hot_goods_center.setImageUrl(img_addr_list[2]);
+				img_hot_goods_left.setImageUrl(img_addr_list[0]);
+				// animationController.scaleIn(v, durationMillis, delayMillis);
+				j = j + 1;
+				break;
+			case (2):
+				img_hot_goods_right.setAnimation(animation);
+				img_hot_goods_center.setAnimation(animation);
+				img_hot_goods_left.setAnimation(animation);
+				img_hot_goods_right.setImageUrl(img_addr_list[2]);
+				img_hot_goods_center.setImageUrl(img_addr_list[0]);
+				img_hot_goods_left.setImageUrl(img_addr_list[1]);
+				// animationController.scaleIn(v, durationMillis, delayMillis);
+				j = 0;
+				break;
+			default:
+				return;
+			}
+
+			if (true) {
+				handler.postDelayed(runnable, 6000);
+			}
+		}
+
+	};
+
+	public Handler handler = new Handler();
 }
--- src/com/example/mainpro/MainActivity.java
+++ src/com/example/mainpro/MainActivity.java
@@ -15,13 +15,22 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		String[] img_list = {"http://pic7.nipic.com/20100604/4214844_232831115578_2.jpg","http://pic.pingpingw.com/ppfz/01/2010091903542125493.jpeg","http://pica.nipic.com/2008-03-19/200831911642647_2.jpg"};
+		String img_addr = "http://h.hiphotos.baidu.com/pic/w%3D230/sign=6d39bd8b908fa0ec7fc7630e1696594a/c8ea15ce36d3d539b9e9707b3b87e950342ab0b5.jpg;"+
+				"http://g.hiphotos.baidu.com/pic/w%3D230/sign=128d995ad788d43ff0a996f14d1fd2aa/f636afc379310a555d97e0dbb64543a982261069.jpg;"+
+				"http://g.hiphotos.baidu.com/pic/w%3D230/sign=d45ce6e20df431adbcd2443a7b37ac0f/bd315c6034a85edfeb5afd5348540923dc5475ef.jpg,"+
+				"http://c.hiphotos.baidu.com/pic/w%3D230/sign=a34c4615b219ebc4c078719ab227cf79/0bd162d9f2d3572ccba2e5ac8b13632762d0c37a.jpg;"+
+				"http://g.hiphotos.baidu.com/pic/w%3D230/sign=ad1de12edcc451daf6f60be886fc52a5/cefc1e178a82b901fc58ba43728da9773812efcd.jpg;"+
+				"http://g.hiphotos.baidu.com/pic/w%3D230/sign=6d19654f0eb30f24359aeb00f894d192/4bed2e738bd4b31c91915a9586d6277f9e2ff86b.jpg,"+
+				"http://h.hiphotos.baidu.com/pic/w%3D230/sign=a81648b68326cffc692ab8b189014a7d/10dfa9ec8a1363277d83fc1c908fa0ec08fac7b6.jpg;"+
+				"http://d.hiphotos.baidu.com/pic/w%3D230/sign=2129d9bd0df431adbcd2443a7b37ac0f/bd315c6034a85edf1e2fc20c48540923dd547579.jpg;"+
+				"http://h.hiphotos.baidu.com/pic/w%3D230/sign=633f80c8eaf81a4c2632ebcae72b6029/77094b36acaf2eddccc07b558c1001e93901935c.jpg;";
+		String[] img_list = img_addr.split(",");
 		mAdapter = new ViewpagerAdapter(getSupportFragmentManager(),img_list);
         mPager = (ViewPager)findViewById(R.id.pager);
         mPager.setAdapter(mAdapter);
         mIndicator = (UnderlinePageIndicator)findViewById(R.id.indicator);
         mIndicator.setViewPager(mPager);
-        mIndicator.setFades(false);
+        mIndicator.setFades(true);
         mIndicator.setCurrentItem(0);
 	}
 
